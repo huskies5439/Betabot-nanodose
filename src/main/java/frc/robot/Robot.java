d486adbfeb2df88cc9e3ab7f65afeb0fb87d8b89
Huskies Prog <57733100+HuskiesProg@users.noreply.github.com>
2020-01-22 18:48:32 -0500

    test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,6 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
-// Je suis gentil
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Preferences;
