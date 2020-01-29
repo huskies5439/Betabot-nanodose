b82e02f67b4a86091fd07b3cbbc50582b50418c6
huskies5439 <57733100+HuskiesProg@users.noreply.github.com>
2020-01-29 18:06:53 -0500

    verif sync 2

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 package frc.robot;
-//😀😀
+//😀
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
